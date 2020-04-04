03d4710f8e0339716070589b564f090adadedbee
CosetteR <ys708@georgetown.edu>
2020-04-03 21:10:02 -0400

    Basic recipe creation logic created

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ class App extends Component {
   state ={
     recipes: [
       {recipeName: "Name1", description: "dummy text", ingredients: ["Ingredient1", "Ingredient2", "Ingredient3"],
-      procedure: ["Step1", "Step2", "Step3"]},
-      {recipeName: "Name2", description: "dummy text", ingredients: ["Ingredient1", "Ingredient2", "Ingredient3"],
-      procedure: ["Step1", "Step2", "Step3"]},
-      {recipeName: "Name3", description: "dummy text", ingredients: ["Ingredient1", "Ingredient2", "Ingredient3"],
       procedure: ["Step1", "Step2", "Step3"]}
     ]
   }
@@ -20,22 +16,54 @@ class App extends Component {
     const {recipes} = this.state;
     return (
       <div className = "App container">
+      {recipes.map((recipe, index)=>
         <Accordion>
-          {recipes.map((recipe, index)=>(
           <Card>
             <Card.Header>
-                <Accordion.Toggle as ={Button} variant="link" eventKey = "0">
+                <Accordion.Toggle as ={Button} variant="link" eventKey = {index}>
                 {recipe.recipeName}
                 </Accordion.Toggle>
             </Card.Header>
-            <Accordion.Collapse eventKey="0">
+            <Accordion.Collapse eventKey={index}>
               <Card.Body>
                 {recipe.description}
               </Card.Body>
             </Accordion.Collapse>
           </Card>
-          ))}
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as ={Button} variant="link" eventKey = {index}>
+              Ingredients
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey={index}>
+              <Card.Body>
+                <ol>
+                 {recipe.ingredients.map((item)=>(
+                   <li key={item}>{item}</li>
+                 ))}
+                </ol>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as ={Button} variant="link" eventKey = {index}>
+              Procedure
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey={index}>
+              <Card.Body>
+                <ol>
+                 {recipe.procedure.map((item)=>(
+                   <li key={item}>{item}</li>
+                 ))}
+                </ol>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
         </Accordion>
+        )}
       </div>
     );
   }
